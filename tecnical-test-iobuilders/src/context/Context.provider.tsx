8b076726d752f89a0290f86ee83d0b1cf32f46d3
Antonio LS <antonio.lopez@babelgroup.com>
2022-06-10 00:46:15 +0200

    App context

---
+++ tecnical-test-iobuilders/src/context/Context.provider.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import AppContext, { AppContextModel } from "./context.model";
+import React from "react";
+
+interface AppContextProviderProps {
+  children?: React.ReactNode;
+}
+
+const AppContextProvider: React.FC<AppContextProviderProps> = ({
+  children,
+}) => {
+  const [user, setUser] = useState("");
+  const [transactions, setTransactions] = useState<any>([]);
+  const db = getFirestore();
+
+  const getUserTransactions = (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        console.log("user", user);
+
+        const q1 = query(
+          collection(db, "transactions"),
+          where("origin", "==", user)
+        );
+        const q2 = query(
+          collection(db, "transactions"),
+          where("destinatary", "==", user)
+        );
+
+        const querySnapshot1 = await getDocs(q1);
+        const querySnapshot2 = await getDocs(q2);
+        Promise.all([querySnapshot1, querySnapshot2]).then((values) => {
+          const data1 = values[0].docs.map((doc) => doc.data());
+          const data2 = values[1].docs.map((doc) => doc.data());
+          const data = [...data1, ...data2];
+          setTransactions(data);
+          resolve(data);
+        });
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    });
+  };
+
+  const contextModel: AppContextModel = {
+    db,
+    user,
+    setUser,
+    getUserTransactions,
+    transactions,
+  };
+
+  return (
+    <AppContext.Provider value={contextModel}>{children}</AppContext.Provider>
+  );
+};
+
+export function useAppContext() {
+  const context = React.useContext(AppContext);
+  if (context === undefined) {
+    throw new Error("useAppContext must be used within a AppContextProvider");
+  }
+  return context;
+}
+
+export default AppContextProvider;
